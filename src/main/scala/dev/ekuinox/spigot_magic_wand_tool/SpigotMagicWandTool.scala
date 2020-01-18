c6d1155a9f6c34f9a400db1696e16378303388f5
ekuinox <depkey@me.com>
2020-01-18 18:56:14 +0900

    #1 コマンド経由でアイテムを取得したい
    ただ、これsetExecutorで死にます

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/SpigotMagicWandTool.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/SpigotMagicWandTool.scala
@@ -4,4 +4,9 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class SpigotMagicWandTool extends JavaPlugin {
 
+  override def onEnable(): Unit = {
+    super.onEnable()
+    getCommand("magicwand").setExecutor(new CommandExecutor)
+  }
+
 }
