abb267bebc804c3b647b50859426cddf09e721d6
ekuinox <depkey@me.com>
2020-01-20 02:05:18 +0900

    #3 InteractEventが2度呼ばれるためのタイマ

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/PlayerInteractEventListener.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/PlayerInteractEventListener.scala
@@ -1,9 +1,30 @@
 package dev.ekuinox.spigot_magic_wand_tool
 
+import org.bukkit.entity.Player
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.{EventHandler, Listener}
+import org.bukkit.metadata.FixedMetadataValue
+import org.bukkit.scheduler.BukkitRunnable
 
 class PlayerInteractEventListener(plugin: SpigotMagicWandTool) extends Listener {
+
+  /**
+   * InteractEventが同時に2回呼ばれるのを防ぐため
+   */
+  object InteractEventTimer {
+    private val KEY = "MAGIC_WAND_INTERACT_EVENT_TIMER"
+    private val DELAY = 10
+    implicit def convert(player: Player) = new {
+      def isActiveTimer: Boolean = try { player.getMetadata(InteractEventTimer.KEY).get(0).asBoolean() } catch { case _: Throwable => false }
+      def enableTimer(): Unit = {
+        player.setMetadata(KEY, new FixedMetadataValue(plugin, true))
+        new BukkitRunnable {
+          override def run(): Unit = player.setMetadata(KEY, new FixedMetadataValue(plugin, false))
+        }.runTaskLater(plugin, DELAY)
+      }
+    }
+  }
+  import InteractEventTimer._
 
   /**
    * このListenerをPluginManagerに登録する
