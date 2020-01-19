4a310068ecb7d9071410af8d1e71f3b7b81ff6e3
ekuinox <depkey@me.com>
2020-01-19 22:09:56 +0900

    #1 失敗してほしいときのテストも追加

--- src/test/scala/MagicWandSpecs.scala
+++ src/test/scala/MagicWandSpecs.scala
@@ -1,6 +1,8 @@
 import java.util
 
 import dev.ekuinox.spigot_magic_wand_tool.MagicWand
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import org.scalatest._
 import org.specs2.mock.Mockito
@@ -16,6 +18,18 @@ class MagicWandSpecs extends FunSuite with Mockito {
     magicWandMock.getType returns MagicWand.MATERIAL
 
     assert(MagicWand.isMatches(magicWandMock))
+  }
+
+  test("is incorrect magic wand") {
+    val itemMetaMock = mock[ItemMeta]
+    val itemMock = mock[ItemStack]
+
+    itemMetaMock.getDisplayName returns ""
+    itemMock.getItemMeta returns itemMetaMock
+    itemMock.getType returns Material.ACACIA_BOAT
+
+    assert(!MagicWand.isMatches(itemMock))
+
   }
 
 }
