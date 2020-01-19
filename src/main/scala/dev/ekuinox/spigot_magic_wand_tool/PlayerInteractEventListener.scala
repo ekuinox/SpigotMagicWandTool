8f438489cfb2df6d9b526e00258f4534c33f889b
ekuinox <depkey@me.com>
2020-01-20 01:43:18 +0900

    #3 ListenerにPluginを投げて生成し、onEnableでListener側からregisterEventsする

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/PlayerInteractEventListener.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/PlayerInteractEventListener.scala
@@ -3,7 +3,14 @@ package dev.ekuinox.spigot_magic_wand_tool
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.{EventHandler, Listener}
 
-class PlayerInteractEventListener extends Listener {
+class PlayerInteractEventListener(plugin: SpigotMagicWandTool) extends Listener {
+
+  /**
+   * このListenerをPluginManagerに登録する
+   */
+  def register(): Unit = {
+    plugin.getServer.getPluginManager.registerEvents(this, plugin)
+  }
 
   @EventHandler
   def onPlayerInteract(event: PlayerInteractEvent): Unit = {
--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/SpigotMagicWandTool.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/SpigotMagicWandTool.scala
@@ -3,11 +3,12 @@ package dev.ekuinox.spigot_magic_wand_tool
 import org.bukkit.plugin.java.JavaPlugin
 
 class SpigotMagicWandTool extends JavaPlugin {
+  val playerInteractEventListener = new PlayerInteractEventListener(this)
 
   override def onEnable(): Unit = {
     super.onEnable()
     getCommand("magicwand").setExecutor(new CommandExecutor)
-    getServer.getPluginManager.registerEvents(new PlayerInteractEventListener(), this)
+    playerInteractEventListener.register()
   }
 
 }
