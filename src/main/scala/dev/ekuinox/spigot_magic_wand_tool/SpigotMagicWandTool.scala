82f13a59057b1a6d7ba3b60b2f2222d1426df959
ekuinox <depkey@me.com>
2020-01-20 14:31:11 +0900

    #4 アンチパターンっぽいけどgetInstanceがしたい...

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/SpigotMagicWandTool.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/SpigotMagicWandTool.scala
@@ -3,12 +3,23 @@ package dev.ekuinox.spigot_magic_wand_tool
 import org.bukkit.plugin.java.JavaPlugin
 
 class SpigotMagicWandTool extends JavaPlugin {
+  import SpigotMagicWandTool._
+
+  instance = Some(this)
   val playerInteractEventListener = new PlayerInteractEventListener(this)
+
+
 
   override def onEnable(): Unit = {
     super.onEnable()
     getCommand("magicwand").setExecutor(new CommandExecutor(this))
     playerInteractEventListener.register()
   }
+
+}
+
+object SpigotMagicWandTool {
+  private var instance: Option[SpigotMagicWandTool] = None
+  def getInstance: Option[SpigotMagicWandTool] = instance
 
 }
