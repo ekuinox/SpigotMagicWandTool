ac6ed197e0b1e2aa5624c6d09dfda3df56f2838d
ekuinox <depkey@me.com>
2020-01-20 13:22:36 +0900

    #3 Permissionを列挙

---
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/permisisons/Permission.scala
@@ -0,0 +1,3 @@
+package dev.ekuinox.spigot_magic_wand_tool.permisisons
+
+class Permission(permission: String) extends org.bukkit.permissions.Permission(s"magicwand.$permission")
---
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/permisisons/Set.scala
@@ -0,0 +1,3 @@
+package dev.ekuinox.spigot_magic_wand_tool.permisisons
+
+object Set extends Permission("set")
