ecf0904eedeaa2470dc07bab6bb9fbbe60c90053
ekuinox <depkey@me.com>
2020-01-22 00:12:07 +0900

    #9 DataContainerからデータを取得したり格納したりするために

---
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/Location.scala
@@ -0,0 +1,28 @@
+package dev.ekuinox.spigot_magic_wand_tool
+
+import org.bukkit.NamespacedKey
+import org.bukkit.persistence.{PersistentDataContainer, PersistentDataType}
+
+case class Location(index: Int, x: Int, y: Int, z: Int) {
+  def toList = List(index, x, y, z)
+}
+
+case object Location {
+  private lazy val NAMESPACED_KEY: NamespacedKey = SpigotMagicWandTool.getInstance.get.makeNamespacedKey("Locations")
+  private val CONTAINER_DATA_TYPE = PersistentDataType.INTEGER_ARRAY
+  private val ARRAY_GROUPED_SIZE = 4
+
+  def apply(location: Array[Int]): Location = new Location(location(0), location(1), location(2), location(3))
+
+  def getLocations(container: PersistentDataContainer): Option[List[Location]] = {
+    if (!container.has(NAMESPACED_KEY, CONTAINER_DATA_TYPE)) return None
+    val data = container.get(NAMESPACED_KEY, CONTAINER_DATA_TYPE)
+    if (data.length % ARRAY_GROUPED_SIZE != 0) return None
+    Some(data.grouped(ARRAY_GROUPED_SIZE).toList.map(Location(_)))
+  }
+
+  def storeLocations(container: PersistentDataContainer, locations: List[Location]): Unit = {
+    container.set(NAMESPACED_KEY, CONTAINER_DATA_TYPE, locations.flatMap(_.toList).toArray)
+  }
+
+}
