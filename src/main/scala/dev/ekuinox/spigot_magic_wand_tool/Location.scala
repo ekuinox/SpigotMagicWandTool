ef98402baabf597e65e8603b7a012f6eef7d4eaf
ekuinox <depkey@me.com>
2020-01-22 01:24:53 +0900

    #9 indexいらねえわ

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/Location.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/Location.scala
@@ -4,18 +4,18 @@ import org.bukkit.NamespacedKey
 import org.bukkit.persistence.{PersistentDataContainer, PersistentDataType}
 import org.bukkit.{Location => BukkitLocation}
 
-case class Location(index: Int, x: Int, y: Int, z: Int) {
-  def toList = List(index, x, y, z)
+case class Location(x: Int, y: Int, z: Int) {
+  def toList = List(x, y, z)
 }
 
 case object Location {
   private lazy val NAMESPACED_KEY: NamespacedKey = SpigotMagicWandTool.getInstance.get.makeNamespacedKey("Locations")
   private val CONTAINER_DATA_TYPE = PersistentDataType.INTEGER_ARRAY
-  private val ARRAY_GROUPED_SIZE = 4
+  private val ARRAY_GROUPED_SIZE = 3
 
-  def apply(location: Array[Int]): Location = new Location(location(0), location(1), location(2), location(3))
+  def apply(location: Array[Int]): Location = new Location(location(0), location(1), location(2))
 
-  def apply(index: Int, location: BukkitLocation) = new Location(index, location.getX.toInt, location.getY.toInt, location.getZ.toInt)
+  def apply(location: BukkitLocation) = new Location(location.getX.toInt, location.getY.toInt, location.getZ.toInt)
 
   def getLocations(container: PersistentDataContainer): Option[List[Location]] = {
     if (!container.has(NAMESPACED_KEY, CONTAINER_DATA_TYPE)) return None
