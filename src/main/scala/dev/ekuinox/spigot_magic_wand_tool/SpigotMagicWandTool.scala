eed85fbf7ede3a855e5112c133367ea9fca7dae2
ekuinox <depkey@me.com>
2020-01-23 01:37:13 +0900

    #16 Listener の登録

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/SpigotMagicWandTool.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/SpigotMagicWandTool.scala
@@ -1,5 +1,6 @@
 package dev.ekuinox.spigot_magic_wand_tool
 
+import dev.ekuinox.spigot_magic_wand_tool.listener.{PlayerInteractEventListener, PlayerItemHeldEventListener}
 import org.bukkit.NamespacedKey
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -8,12 +9,14 @@ class SpigotMagicWandTool extends JavaPlugin {
 
   instance = Some(this)
   val playerInteractEventListener = new PlayerInteractEventListener(this)
+  val playerItemHeldEventListener = new PlayerItemHeldEventListener(this)
   val particleManager = new particle.Manager(this)
   
   override def onEnable(): Unit = {
     super.onEnable()
     getCommand("magicwand").setExecutor(new CommandExecutor(this))
     playerInteractEventListener.register()
+    playerItemHeldEventListener.register()
   }
 
   def makeNamespacedKey(key: String) = new NamespacedKey(this, key)
