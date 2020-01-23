72b82a7dd162096ee6d6fbf2ce72c3475caa13ec
ekuinox <depkey@me.com>
2020-01-23 13:23:39 +0900

    #16 ログイン/ログアウト時にパーティクルを消したりつけたりする

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/listener/PlayerLoginEventListener.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/listener/PlayerLoginEventListener.scala
@@ -1,6 +1,6 @@
 package dev.ekuinox.spigot_magic_wand_tool.listener
 
-import dev.ekuinox.spigot_magic_wand_tool.SpigotMagicWandTool
+import dev.ekuinox.spigot_magic_wand_tool.{LocationsManager, SpigotMagicWandTool}
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerLoginEvent
 
@@ -8,7 +8,9 @@ class PlayerLoginEventListener(plugin: SpigotMagicWandTool) extends Listener(plu
 
   @EventHandler
   def onPlayerLogin(event: PlayerLoginEvent): Unit = {
-    
+    for { locations <- LocationsManager.get(event.getPlayer) } {
+      locations.foreach(plugin.particleManager.startParticle(event.getPlayer, event.getPlayer.getWorld,  _))
+    }
   }
 
 }
--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/listener/PlayerQuitEventListener.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/listener/PlayerQuitEventListener.scala
@@ -1,6 +1,6 @@
 package dev.ekuinox.spigot_magic_wand_tool.listener
 
-import dev.ekuinox.spigot_magic_wand_tool.SpigotMagicWandTool
+import dev.ekuinox.spigot_magic_wand_tool.{LocationsManager, SpigotMagicWandTool}
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.PlayerQuitEvent
 
@@ -8,7 +8,9 @@ class PlayerQuitEventListener(plugin: SpigotMagicWandTool) extends Listener(plug
 
   @EventHandler
   def onPlayerQuit(event: PlayerQuitEvent): Unit = {
-
+    for { locations <- LocationsManager.get(event.getPlayer) } {
+      locations.foreach(location => plugin.particleManager.stopParticle(event.getPlayer, location))
+    }
   }
 
 }
