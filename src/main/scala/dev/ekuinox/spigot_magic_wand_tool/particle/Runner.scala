0d1ece167e212188ee05cfbee031cdbd30b12eac
ekuinox <depkey@me.com>
2020-01-23 12:22:39 +0900

    #16 パーティクルを1つだけ出す

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/particle/Runner.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/particle/Runner.scala
@@ -1,13 +1,13 @@
 package dev.ekuinox.spigot_magic_wand_tool.particle
 
 import dev.ekuinox.spigot_magic_wand_tool.{Location, SpigotMagicWandTool}
-import org.bukkit.{Effect, World}
+import org.bukkit.World
 import org.bukkit.scheduler.BukkitRunnable
 
 class Runner(plugin: SpigotMagicWandTool, world: World, location: Location) extends BukkitRunnable {
   import Manager._
 
   override def run(): Unit = {
-    world.spawnParticle(PARTICLE_TYPE, location.x.toDouble + 0.5, location.y.toDouble - 0.5f, location.z.toDouble + 0.5, PARTICLE_COUNT)
+    world.spawnParticle(PARTICLE_TYPE, location.toBukkitLocation(world).add(0.5, 0.5, 0.5), 0, 0, 0, 0, 0)
   }
 }
