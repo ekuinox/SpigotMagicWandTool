32a6bb7f8db48f6a62f5fb4f37d430a47291f4b0
ekuinox <depkey@me.com>
2020-01-23 10:08:43 +0900

    #16 数を減らしている

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/particle/Manager.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/particle/Manager.scala
@@ -23,7 +23,7 @@ class Manager(plugin: SpigotMagicWandTool) {
 
 object Manager {
   val PARTICLE_TYPE = Particle.SPELL_WITCH
-  val PARTICLE_COUNT = 100
+  val PARTICLE_COUNT = 10
   val PARTICLE_TICK_SPAN = 10
 
 }
