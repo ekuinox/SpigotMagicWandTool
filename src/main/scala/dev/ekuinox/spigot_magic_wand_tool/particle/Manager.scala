fbb11ba494e0c363e5642ffa6271ed619cba08be
ekuinox <depkey@me.com>
2020-01-23 23:10:51 +0900

    #24 コメントの追記

--- src/main/scala/dev/ekuinox/spigot_magic_wand_tool/particle/Manager.scala
+++ src/main/scala/dev/ekuinox/spigot_magic_wand_tool/particle/Manager.scala
@@ -12,16 +12,38 @@ class Manager(plugin: SpigotMagicWandTool) {
 
   private val runners: Map[(Player, Location, World), Runner] = Map[(Player, Location, World), Runner]()
 
+  /**
+   * パーティクルを一つ湧かす Runner の開始
+   * @param player Player
+   * @param world World
+   * @param location Location
+   */
   def startParticle(player: Player, world: World, location: Location): Unit = {
     val runner = new Runner(plugin, world, location)
     runner.runTaskTimerAsynchronously(plugin, 0, PARTICLE_TICK_SPAN)
     runners += ((player, location, world) -> runner)
   }
 
+  /**
+   * Player のいる World を元に startParticle を行う
+   * @param player Player
+   * @param location Location
+   */
   def startParticle(player: Player, location: Location): Unit = startParticle(player, player.getWorld, location)
 
+  /**
+   * Runner の停止
+   * @param player Player
+   * @param world World
+   * @param location Location
+   */
   def stopParticle(player: Player, world: World, location: Location): Unit = runners.remove((player, location, world)).foreach(_.cancel())
 
+  /**
+   * Player のいる World を元に stopParticle を行う
+   * @param player Player
+   * @param location Location
+   */
   def stopParticle(player: Player, location: Location): Unit = stopParticle(player, player.getWorld, location)
 
 }
